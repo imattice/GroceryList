eacf82976206ff09e88bb1903b4ce5f2f70406ab
Ike Mattice <ikemattice@icloud.com>
2019-10-24 20:48:31 -0700

    Initial Commit

---
+++ GroceryListTests/GroceryListTests.swift
@@ -0,0 +1,34 @@
+//
+//  GroceryListTests.swift
+//  GroceryListTests
+//
+//  Created by Ike Mattice on 10/24/19.
+//  Copyright © 2019 Ike Mattice. All rights reserved.
+//
+
+import XCTest
+@testable import GroceryList
+
+class GroceryListTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
